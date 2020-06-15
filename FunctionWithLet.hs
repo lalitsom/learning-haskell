3cc636849eb897a0c9d8da9016ff7f81a5f11336
lalitsom27 <lalitsom27@gmail.com>
2020-06-15 20:57:28 +0530

    basics added

---
+++ FunctionWithLet.hs
@@ -0,0 +1,4 @@
+module FunctionWithWhere where
+
+printInc2 n = let plusTwo = n+2
+    in print plusTwo
---
+++ functionWithWhere.hs
@@ -0,0 +1,8 @@
+module FunctionWithWhere where
+
+printInc n = print plusTwo
+    where plusTwo = n+2
+
+mult1  = x*y
+    where x=5
+          y=6    
